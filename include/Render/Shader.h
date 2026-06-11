deb97f574be81400430e9f81c44fe1a33fdf6644
melty <Yusei.k.0403@gmail.com>
2026-06-12 02:16:47 +0900

    implemented shader

--- include/Render/Shader.h
+++ include/Render/Shader.h
@@ -11,9 +11,13 @@ class Shader {
 public:
 	Shader(const std::string& vertPath, const std::string& fragPath);
 
+	void Use();
 
 private:
 	std::string ReadFile(const std::string& shaderPath);
 
 	unsigned int CompileShader(unsigned int type, const std::string& source);
+
+private:
+	unsigned int m_id = -1;
 };
--- src/Render/Shader.cpp
+++ src/Render/Shader.cpp
@@ -1,4 +1,5 @@
 #include "Render/Shader.h"
+#include <iostream>
 
 Shader::Shader(const std::string& vertPath, const std::string& fragPath) {
 	std::string vertSource = ReadFile(vertPath);
@@ -12,8 +13,19 @@ Shader::Shader(const std::string& vertPath, const std::string& fragPath) {
 	glAttachShader(program, fragShader);
 
 	glLinkProgram(program);
+	int success;
+	glGetProgramiv(program, GL_LINK_STATUS, &success);
 
+	if (!success) {
+		char infoLog[1024];
+		glGetProgramInfoLog(program, 1024, nullptr, infoLog);
+		std::cerr << "Shader link error:\n" << infoLog << std::endl;
+	}
 
+	m_id = program;
+
+	glDeleteShader(vertShader);
+	glDeleteShader(fragShader);
 }
 
 
@@ -37,5 +49,31 @@ unsigned int Shader::CompileShader(unsigned int type, const std::string& source)
 	glShaderSource(shader, 1, &src, nullptr);
 	glCompileShader(shader);
 
+	int success;
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+
+	if (!success) {
+		char infoLog[1024];
+		glGetShaderInfoLog(shader, 1024, nullptr, infoLog);
+
+		if (type == GL_VERTEX_SHADER) {
+			std::cerr << "Vertex shader compile error:\n";
+		}
+		else if (type == GL_FRAGMENT_SHADER) {
+			std::cerr << "Fragment shader compile error:\n";
+		}
+		else {
+			std::cerr << "Shader compile error:\n";
+		}
+
+		std::cerr << infoLog << std::endl;
+	}
+
+
 	return shader;
+}
+
+
+void Shader::Use() {
+	glUseProgram(m_id);
 }
